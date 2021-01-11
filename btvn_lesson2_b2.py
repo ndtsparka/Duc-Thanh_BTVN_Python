f2a0cb57a5924dd3cc38c795f24067fbf3a8c270
Nguyen Duc Thanh <ndt.sparka2003@gmail.com>
2021-01-12 01:54:28 +1000

    a

---
+++ btvn_lesson2_b2.py
@@ -0,0 +1,9 @@
+a = int(input("Nhap so"))
+b = int(input("Nhap so"))
+phep_mu = a**b
+equa = "a mu b = "+ str(phep_mu)
+print(equa)
+
+phep_chia_lay_du = a%b
+equa2 = "ket qua cua phep chia lay phan nguyen a chia cho b la: " + str(phep_chia_lay_du)
+print(equa2)
