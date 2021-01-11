df590c8c86d760384e567c15c2c3eb24dee7e906
Nguyen Duc Thanh <ndt.sparka2003@gmail.com>
2021-01-12 01:53:54 +1000

    w

---
+++ btvn_lesson2_b1.py
@@ -0,0 +1,9 @@
+name = str(input("Nhap ten"))
+intro_name = "Toi ten la " + (name)
+print(intro_name)
+t = len(intro_name)
+print (intro_name[11: (t)])
+
+age = str(input("Nhap tuoi"))
+intro_nameplusage = (intro_name)+". Toi " + (age) +" tuoi"
+print(intro_nameplusage) 
